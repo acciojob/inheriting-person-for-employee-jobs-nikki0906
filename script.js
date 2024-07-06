44b637d423f49ce8e0b521bd9a37d59987a90de6
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-07-06 10:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 // complete this js code
 /*  your css code here. If applicable */
-function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
 
-// Do not change code below this line
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+
+function Employee(name, age, jobTitle) {
+    Person.call(this, name, age); 
+    this.jobTitle = jobTitle;
+}
+
+// Inherit from Person
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
+
 window.Person = Person;
 window.Employee = Employee;
+
+const person = new Person("Alice", 25);
+person.greet(); 
+
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet(); 
